3b595f139bc1867b61578c4a8891e8ae3e787cec
NatureSon22 <blendingworker@gmail.com>
2024-12-20 13:29:16 +0800

    commit

---
+++ client/src/utils/formatDate.js
@@ -0,0 +1,9 @@
+const formatDate = (date) => {
+  return new Date(date).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  });
+};
+
+export default formatDate;
