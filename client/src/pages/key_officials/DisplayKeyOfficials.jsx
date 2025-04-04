32dc1410c0e9f6999b6c3ad871b9439f038bc14a
Kenneth San Pedro <kennethsanpedro1108@gmail.com>
2025-04-04 20:45:46 +0800

    Commit

--- client/src/pages/key_officials/DisplayKeyOfficials.jsx
+++ client/src/pages/key_officials/DisplayKeyOfficials.jsx
@@ -97,10 +97,23 @@ const DisplayingKeyOfficials = () => {
         </p>
       </div>
 
-      <div className="mt-12 py-6">
-        <div className="grid grid-cols-1 gap-7 md:grid-cols-2 lg:grid-cols-5">
+      <div className="mt-12 py-6 px-4 sm:px-6 lg:px-8"> {/* Added horizontal padding for smaller screens */}
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
           {filteredOfficials?.map((official, index) => (
-            <OfficialCard key={index} official={official} />
+            <div className="box-shadow-200 flex px-2 flex-col items-center rounded-md border border-secondary-200/30 bg-white py-4">
+            <img
+              src={official.key_official_photo_url}
+              alt={official.name}
+              className="h-[200px] w-[210px] rounded-md object-cover"
+              loading="lazy"
+            />
+            <h3 className="text-md font-cizel-decor mt-5 text-[1.05rem] px-4 text-center font-bold text-gray-800">
+              {official.name}
+            </h3>
+            <p className="mt-2 font-cizel text-center text-gray-600">
+              {official.position_name}
+            </p>
+          </div>
           ))}
         </div>
       </div>
